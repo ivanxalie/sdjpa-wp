fc109478599e2c0b6491b320235112ff79eadf3a
ivanxalie <ivanxalie@gmail.com>
2025-01-07 21:52:18 +0300

    Added lombok and User entity

---
+++ src/main/java/guru/springframework/sdjpa/wp/domain/User.java
@@ -0,0 +1,46 @@
+package guru.springframework.sdjpa.wp.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@SuppressWarnings("ALL")
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "wp_users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_login")
+    private String login;
+
+    @Column(name = "user_pass")
+    private String password;
+
+    @Column(name = "user_nicename")
+    private String nicename;
+
+    @Column(name = "user_email")
+    private String email;
+
+    @Column(name = "user_url")
+    private String url;
+
+    @Column(name = "user_registered")
+    private Timestamp registered;
+
+    @Column(name = "user_activation_key")
+    private String activationKey;
+
+    @Column(name = "user_status")
+    private Integer status;
+
+    private String displayName;
+}
